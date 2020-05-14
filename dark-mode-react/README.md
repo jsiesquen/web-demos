4d22920987395cb729611eeef74f781c1cf2ecda
Juan Siesquen <juan.siesquen@gmail.com>
2020-05-14 03:18:33 -0500

    Complete darkmode functionality

--- dark-mode-react/README.md
+++ dark-mode-react/README.md
@@ -1,6 +1,6 @@
 # Documentation #
 
 ## Main Output
-![Dark Mode on React](https://jsiesquen.github.io/web-demos/dark-mode-react/documentation/dark-mode-react.png)
+![Dark Mode on React](documentation/dark-mode-react.png)
 
 Hosted project [Dark Mode on React](https://jsiesquen.github.io/web-demos/dark-mode-react/)
--- dark-mode-react/src/component/switch.js
+++ dark-mode-react/src/component/switch.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import './switch.css'
 
 function Switch() {
+    const ref = useRef(null);
+    function handleDarkModeChange(event) {
+        // 1 method: event.target.checked
+        // 2 method: ref.current.checked; useRef using hook
+        if (ref.current.checked) {
+            document.body.classList.remove('is-light-mode');
+            document.body.classList.add('is-dark-mode');
+        } else {
+            document.body.classList.remove('is-dark-mode');
+            document.body.classList.add('is-light-mode');
+        }
+    }
+
+    useEffect(() => {
+        if (window.matchMedia('(prefers-colors-scheme: dark)').matches) {
+            ref.current.setAttribute("checked", true)
+        }
+    }, [])  // 2° param: When this component each rendering only
+
     return (
             <div className="dark-mode">
                 <p className="dark-mode-title">Dark Mode</p>
-                <input type="checkbox" className="checkbox" name="checkbox" id="checkbox" />
+                <input ref={ref} onChange={handleDarkModeChange} type="checkbox" className="checkbox" name="checkbox" id="checkbox" />
                 <label className="switch" htmlFor="checkbox"></label>
             </div>
         );
